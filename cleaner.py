351006ede40a07457f9bdeafd51e5bad984fb3eb
astranero <esa.taheresa@gmail.com>
2023-10-07 13:23:12 +0300

    add cleaner

---
+++ cleaner.py
@@ -0,0 +1,58 @@
+from testscraper import search_jobs
+import string
+from nltk.stem.snowball import SnowballStemmer
+import pandas as pd
+from langdetect import detect
+
+
+def get_jobs(keywords:list):
+    df = search_jobs(keywords)
+    return df
+
+
+def description_remove_stopwords(description):
+    description = description.str.lower()
+    stopwords = open("stopwords.txt", "r")
+    data = stopwords.read()
+    stopwords_list = data.split("\n")
+    description.fillna("", inplace=True)
+    description = description.apply(lambda x: "".join([c for c in x if c not in string.punctuation]))
+    description = description.apply(lambda text: " ".join([word for word in text.split() if word not in stopwords_list]))
+    return description
+
+def stem_text(text):
+    lang = detect(text)
+    stemmer = SnowballStemmer(lang if lang in SnowballStemmer.languages else "english")
+    return " ".join([stemmer.stem(word) for word in text.split()])
+
+
+def stem_description(description):
+    description = description.apply(stem_text) 
+    return description
+
+
+def clean_description(df):
+    description = df["description"]
+    description_no_stopwords = description_remove_stopwords(description)
+    stemmed_description = stem_description(description_no_stopwords)
+    return stemmed_description
+
+
+def clean_via_search(keywords):
+    df = get_jobs(list(keywords))
+    description = clean_description(df)
+    df["description"] = description
+    print(description)
+    print(df)
+    
+
+def clean_via_csv():
+    df = pd.read_csv("wantedjobs.csv")
+    description = clean_description(df)
+    df["description"] = description
+    print(description)
+    print(df)
+    
+
+if __name__ == "__main__":
+    clean_via_csv()
--- testscraper.py
+++ testscraper.py
@@ -2,35 +2,58 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 import csv
+import asyncio
+import aiohttp
+import time
 
 def get_user_input():
     search_for = input('Enter the keyword to search for jobs: ')
     return search_for
 
-def scrape_job_ids(keyword):
-    job_ids = []
+
+async def fetch(url:str, headers:str) -> str:
+    async with aiohttp.ClientSession() as session:
+        await asyncio.sleep(10)  
+        async with session.get(url, headers=headers) as response:
+            if response.status == 200:
+                return await response.text()
+            else:
+                print(f"Received status code {response.status}")
+                return None
+
+
+async def scrape_job_ids(keywords: list) -> set:
+    job_ids = set()
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
     }
     base_url = 'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords={}&location=Helsinki%2C%20Uusimaa%2C%20Finland&geoId=106591199&f_E=2&start={}'
     # hardcoded 5 page max
-    for page_number in range(5):
-        url = base_url.format(keyword, page_number * 25)
-        response = requests.get(url, headers=headers)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        job_cards = soup.find_all("li")
-        for card in job_cards:
-            job_id_element = card.find("div", {"class": "base-card"})
-            if job_id_element and 'data-entity-urn' in job_id_element.attrs:
-                job_entity_urn = job_id_element['data-entity-urn']
-                job_id = job_entity_urn.split(":")[3]
-                job_ids.append(job_id)
-            else:
-                print("Job ID element or data-entity-urn attribute not found for this card.")
+
+    tasks = []
+    for keyword in keywords:
+        for page_number in range(5):
+            url = base_url.format(keyword, page_number * 25)
+            task = asyncio.ensure_future(fetch(url, headers))
+            tasks.append(task)
+
+    responses = await asyncio.gather(*tasks)
+    for response in responses:
+        if response:
+            soup = BeautifulSoup(response, 'html.parser')
+            job_cards = soup.find_all("li")
+            for card in job_cards:
+                job_id_element = card.find("div", {"class": "base-card"})
+                if job_id_element and 'data-entity-urn' in job_id_element.attrs:
+                    job_entity_urn = job_id_element['data-entity-urn']
+                    job_id = job_entity_urn.split(":")[3]
+                    job_ids.add(job_id)
+                else:
+                    print("Job ID element or data-entity-urn attribute not found for this card.")
     return job_ids
 
 
-def scrape_job_details(job_ids):
+def scrape_job_details(job_ids: list) -> list:
     jobs = []
     target_url = 'https://www.linkedin.com/jobs-guest/jobs/api/jobPosting/{}'
     for job_id in job_ids:
@@ -65,12 +88,55 @@ def scrape_job_details(job_ids):
             jobs.append(job)
     return jobs
 
-def save_to_csv(data):
-    df = pd.DataFrame(data)
-    df.to_csv('wantedjobs.csv', index=False, encoding='utf-8', quoting=csv.QUOTE_ALL)
+
+def save_to_csv(data: pd.DataFrame):
+    data.to_csv('wantedjobs.csv', index=False, encoding='utf-8', quoting=csv.QUOTE_ALL)
+
+
+def search_jobs(*keywords: str) -> pd.DataFrame:
+    job_ids = asyncio.run(scrape_job_ids(list(keywords)))
+
+    job_details = scrape_job_details(job_ids)
+
+    return pd.DataFrame(job_details)
 
 if __name__ == "__main__":
-    keyword = get_user_input()
-    job_ids = scrape_job_ids(keyword)
-    job_details = scrape_job_details(job_ids)
+    job_details = search_jobs(
+        "devops", 
+        "python", 
+        "developer",
+        "back-end", 
+        "front-end",
+        "full-stack", 
+        "fullstack", 
+        "ios", 
+        "android",
+        "game%20developer",
+        "ui/ux",
+        "cloud",
+        "aws",
+        "azure",
+        "gcp",
+        "data",
+        "data%20engineer",
+        "data%20analyst",
+        "data%20scientist",
+        "ml",
+        "ai",
+        "nlp",
+        "cybersecurity",
+        "network",
+        "qa",
+        "test%20engineer",
+        "systems%20engineer",
+        "embedded",
+        "firmware",
+        "blockchain",
+        "ar/vr",
+        "big%20data",
+        "dba",
+        "rpa",
+        "product%20manager"
+    )
     save_to_csv(job_details)
+
